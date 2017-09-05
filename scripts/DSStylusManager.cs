bd3f68cfc17d42e39cb8615d274acec7b2fed33f
eagleEggs <eric.mitchell.r@gmail.com>
2017-09-05 05:30:42 -0400

    Update DSStylusManager.cs

--- scripts/DSStylusManager.cs
+++ scripts/DSStylusManager.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class DSStylusManager : MonoBehaviour {
 
-	//[SerializeField] private GameObject stylus;
+	[SerializeField] 	private Animator stylusAnimator;
+						private bool stylusAnimationComplete=false;
 
-	void Start(){}
+	void Start(){
 
-	void Update(){
-		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x+25f, Input.mousePosition.y+50f, distance_to_screen ));
+		StartCoroutine (retractStylus());
+
 
 	}
+
+	void Update(){
+
+		if(stylusAnimationComplete){
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x+25f, Input.mousePosition.y+50f, distance_to_screen ));
+	}
+	}
+
+	IEnumerator retractStylus(){
+
+		yield return new WaitForSeconds(0.5f);
+		stylusAnimator.SetTrigger("stylus");
+		yield return new WaitForSeconds(1f);
+		stylusAnimator.SetBool("stylus", false);
+		stylusAnimationComplete=true;
+		stylusAnimator.animatePhysics=true;
+		yield return null;
+
+	}
+
 
 }
