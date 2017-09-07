c0b5bdd3ee1d6087784ebd95600611e9ec2a5c1e
eagleEggs <eric.mitchell.r@gmail.com>
2017-09-06 21:28:37 -0400

    updated stylus offset

--- scripts/DSStylusManager.cs
+++ scripts/DSStylusManager.cs
@@ -18,7 +18,7 @@ public class DSStylusManager : MonoBehaviour {
 
 		if(stylusAnimationComplete){
 		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x+25f, Input.mousePosition.y+50f, distance_to_screen ));
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x+60f, Input.mousePosition.y+50f, distance_to_screen ));
 	}
 	}
 
